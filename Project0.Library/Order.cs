4ad55d4d8ae7af2b52b2d0489f4830c0cb5bbf81
Javon Negahban <javon.negahban@cox.net>
2019-10-04 10:32:03 -0500

    Added Customer, Order, and Product blueprints

---
+++ Project0.Library/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project0.Library
+{
+    class Order
+    {
+        public string StoreLocation { get; set; }
+        public Customer Customer { get; set; }
+        public string OrderTime { get; set; }
+        public List<Product> Products { get; set; }
+        public bool ValidateOrderNotTooLarge()
+        {
+            return true;
+        }
+    }
+}
---
+++ Project0.Library/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace Project0.Library
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
