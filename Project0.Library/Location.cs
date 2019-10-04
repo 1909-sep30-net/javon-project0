38a3e6e538b0333d1e0f677e53394c99f96b4d18
Javon Negahban <javon.negahban@cox.net>
2019-10-04 10:37:02 -0500

    Added location blueprint

---
+++ Project0.Library/Location.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project0.Library
+{
+    class Location
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
