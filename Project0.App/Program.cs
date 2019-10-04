dcbb189c81bec9835c16451ec57ed73c04f536b1
Javon Negahban <javon.negahban@cox.net>
2019-10-04 10:50:29 -0500

    Added basic console interface blueprint

--- Project0.App/Program.cs
+++ Project0.App/Program.cs
@@ -6,7 +6,19 @@ namespace Project0.App
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Javon's Tea Store");
+            Console.WriteLine("Options");
+            Console.WriteLine("1] Add a new customer");
+            Console.WriteLine("What would you like to do?: ");
+            string input = Console.ReadLine();
+            if (input.Equals("1"))
+            {
+                Console.WriteLine("Valid input");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input");
+            }
         }
     }
 }
