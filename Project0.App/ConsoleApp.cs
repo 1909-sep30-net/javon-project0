4fb7cd0861548b03e52592d06849eca54e7afb31
Javon Negahban <javon.negahban@cox.net>
2019-10-04 12:41:32 -0500

    Added third path for searching order details

--- Project0.App/ConsoleApp.cs
+++ Project0.App/ConsoleApp.cs
@@ -10,6 +10,7 @@ namespace Project0.App
             Console.WriteLine("Options");
             Console.WriteLine("1] Add a new customer");
             Console.WriteLine("2] Search for a customer");
+            Console.WriteLine("3] Display details of an order");
             Console.WriteLine("What would you like to do?: ");
             string input = Console.ReadLine();
             if (input.Equals("1"))
@@ -17,6 +18,16 @@ namespace Project0.App
                 Console.WriteLine("What would the name of the customer be?: ");
                 string customerName = Console.ReadLine();
                 Console.WriteLine($"We have added a customer of id 1 with name {customerName}");
+            }
+            else if (input.Equals("2"))
+            {
+                Console.WriteLine("What is the name of the customer you are searching for?");
+            }
+            else if (input.Equals("3"))
+            {
+                Console.WriteLine("What is the order ID?: ");
+                string orderId = Console.ReadLine();
+                Console.WriteLine($"Searching for order {orderId}");
             }
             else
             {
