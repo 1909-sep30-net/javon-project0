9f9a63d1473aa4c49acf97ec9185ffa0308ccf16
Javon Negahban <javon.negahban@cox.net>
2019-10-04 11:04:22 -0500

    Renamed Console to ConsoleApp and added 2nd option for searching customer

---
+++ Project0.App/ConsoleApp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Project0.App
+{
+    class ConsoleApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Javon's Tea Store");
+            Console.WriteLine("Options");
+            Console.WriteLine("1] Add a new customer");
+            Console.WriteLine("2] Search for a customer");
+            Console.WriteLine("What would you like to do?: ");
+            string input = Console.ReadLine();
+            if (input.Equals("1"))
+            {
+                Console.WriteLine("What would the name of the customer be?: ");
+                string customerName = Console.ReadLine();
+                Console.WriteLine($"We have added a customer of id 1 with name {customerName}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input");
+            }
+        }
+    }
+}
