6b8c9038575b3aa024c85105998c96c8230f6027
Javon Negahban <javon.negahban@cox.net>
2019-10-10 10:55:22 -0500

    Updated menu message symbols

--- Project0.App/Menu.cs
+++ Project0.App/Menu.cs
@@ -6,15 +6,15 @@ namespace Project0.App
     {
         internal static void DisplayMenu()
         {
-            Console.WriteLine("Welcome to Javon's Tea Store");
-            Console.WriteLine("0] Add a new customer");
-            Console.WriteLine("1] Search for a customer");
-            Console.WriteLine("2] Display details of an order");
-            Console.WriteLine("3] Display order history of a location");
-            Console.WriteLine("4] Display order history of a customer");
-            Console.WriteLine("5] Display all locations");
-            Console.WriteLine("6] Exit");
-            Console.WriteLine("What would you like to do?: ");
+            Console.WriteLine("[*] Welcome to Javon's Tea Store");
+            Console.WriteLine("[0] Add a new customer");
+            Console.WriteLine("[1] Search for a customer");
+            Console.WriteLine("[2] Display details of an order");
+            Console.WriteLine("[3] Display order history of a location");
+            Console.WriteLine("[4] Display order history of a customer");
+            Console.WriteLine("[5] Display all locations");
+            Console.WriteLine("[6] Exit");
+            Console.WriteLine("[?] What would you like to do");
         }
 
         internal static MenuRequest PromptUser()
