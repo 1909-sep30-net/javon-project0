7c1210692ead5611af2d25387846843d42303c34
Javon Negahban <javon.negahban@cox.net>
2019-10-10 08:55:09 -0500

    Added LocationData to return list of locations

---
+++ Project0.Data/LocationData.cs
@@ -0,0 +1,13 @@
+﻿using Project0.Logic;
+using System.Collections.Generic;
+
+namespace Project0.Data
+{
+    class LocationData
+    {
+        public static List<Location> getLocations()
+        {
+            return MemoryStore.Locations;
+        }
+    }
+}
