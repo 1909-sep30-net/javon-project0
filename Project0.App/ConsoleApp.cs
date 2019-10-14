e519af5171c0fd4bc31c6f601ffa573f63253b99
Javon Negahban <javon.negahban@cox.net>
2019-10-14 16:20:11 -0500

    piece of documentation

--- Project0.App/ConsoleApp.cs
+++ Project0.App/ConsoleApp.cs
@@ -3,12 +3,15 @@ using Serilog;
 
 namespace Project0.App
 {
+    /// <summary>
+    /// The class which holds the entry point to the console application.
+    /// </summary>
     internal class ConsoleApp
     {
         private const string logFile = @"C:\revature\javon-project0\Log.txt";
 
         /// <summary>
-        /// Entry point to the ConsoleApp.
+        /// Entry point to the console application.
         /// </summary>
         /// <param name="args">Arguments to program running</param>
         private static void Main(string[] args)
